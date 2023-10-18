da1a3a0842189a859c5d6df4756d248d34b18ef1
arthur <arthur.rios007@gmail.com>
2023-10-18 14:48:49 -0300

    add types to api response

--- src/services/getCityByNameService.ts
+++ src/services/getCityByNameService.ts
@@ -7,9 +7,23 @@ export interface CityProps {
   longitude: number;
 }
 
+export interface CityAPIResponse {
+  id: string;
+  name: string;
+  sys: {
+    country?: string;
+  }
+  coord: {
+    lat: number;
+    lon: number;
+  }
+}
+
 export async function getCityByNameService(name: string): Promise<CityProps[]> {
   try {
-    const { data } = await api.get(`/weather?q=${name}`);
+    const { data } = await api.get<CityAPIResponse>(`/weather?q=${name}`);
+    console.log(data.sys.country);
+    
 
     const city = {
       id: data.id,
